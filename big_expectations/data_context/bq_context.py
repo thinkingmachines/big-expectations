f097226a2e17fb41f5bfd5d6a9f08ae42abb00b5
iman <syvlabs@users.noreply.github.com>
2018-06-10 14:38:37 +0800

    Update bq_context.py

--- big_expectations/data_context/bq_context.py
+++ big_expectations/data_context/bq_context.py
@@ -1,7 +1,10 @@
 import great_expectations as ge
 from sqlalchemy import create_engine
 
-def get_data_context(uri, credentials_path):
+BQ_URI_FMT = 'bigquery://{}'
+
+def get_data_context(project_id, credentials_path):
+    uri = BQ_URI_FMT.format(project_id)
     context = ge.get_data_context('SqlAlchemy', uri)
     context.engine = create_engine(uri, credentials_path=credentials_path)
     return context
